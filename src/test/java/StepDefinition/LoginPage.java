71a3f6aeea1701fc8e01a4c82a09ef19b81c50f3
KSithole <sibongilesithole71@gmail.com>
2025-02-16 20:50:41 +0200

    added On step def

--- src/test/java/StepDefinition/LoginPage.java
+++ src/test/java/StepDefinition/LoginPage.java
@@ -1,6 +1,9 @@
 package StepDefinition;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import utilities.BrowserDriver;
@@ -17,6 +20,27 @@ public class LoginPage {
 
         String logoText = driver.findElement(By.className("login_logo")).getText();
         Assert.assertEquals(logoText, "Swag Labs", "Page verification failed: Expected 'Swag Labs'");
+    }
+
+
+    @And("I enter username (.*)$")
+    public void i_enter_username_username(String username) {
+        driver.findElement(By.id("user-name")).sendKeys(username);
+    }
+
+    @And("I enter password (.*)$")
+    public void i_enter_password_password(String password) {
+        driver.findElement(By.id("password")).sendKeys(password);
+    }
+
+    @When("I click login button")
+    public void i_click_login_button() {
+        driver.findElement(By.name("login-button")).click();
+    }
+
+    @Then("The home page is displayed")
+    public void the_home_page_is_displayed() {
+        driver.findElement(By.xpath("//span[@class='title'][contains(.,'Products')]")).isDisplayed();
     }
 
 }
